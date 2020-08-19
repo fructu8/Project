09ee141d9facbd31b78b2145f88562b3d719c688
fructu8 <mfructuosob@gmail.com>
2020-08-19 16:26:32 +0200

    Last commit

---
+++ public/client.js
@@ -0,0 +1,40 @@
+	//Make connection to:
+	var socket = io.connect('http://localhost:4000');
+//	var socket = io.connect('http://192.168.1.131:4000');
+
+
+
+//stop button
+	var stop = 0;
+
+  var output = document.getElementById('c');
+
+//Create audio context and oscillator
+	var audioCtx = new (window.webkitAudioContext || window.AudioContext)();
+	var oscillator = audioCtx.createOscillator();
+
+	// Listen for events
+	socket.on('channel', function(data){
+		console.log(data);
+    //Send data to oscillator
+		updateAudiocontext (data);
+		output.innerHTML += data.type + data.frequency;
+	});
+
+	function updateAudiocontext (data, oscillator, audioCtx){
+		window.oscillator;
+		window.audioCtx;
+		window.oscillator.type = data.type;
+		window.oscillator.frequency.setValueAtTime(data.frequency, window.audioCtx.currentTime); // value in hertz
+		window.oscillator.connect(window.audioCtx.destination);
+		window.oscillator.start();
+	}
+
+//stop button listener
+	document.querySelector('button').addEventListener('click', function() {
+	  stop = 0;
+	});
+	document.querySelector('#stop').addEventListener('click', function() {
+		stop = 1;
+		window.oscillator.stop();
+	});
---
+++ public/control.js
@@ -0,0 +1,106 @@
+//Make connection
+var socket = io.connect('http://localhost:4000');
+
+//init config
+var forma = "sine";
+
+
+  $(".key").on("mousedown", function(){
+      updateSound($(this).data('value'), $("#forma").val());
+  });
+
+  $("button").on("mousedown", function(){
+    updateSound($(this).val(), $("#forma").val());
+  });
+
+  $(".key").on("mouseup", function(){
+    updateSound(0, $("#forma").val());
+  });
+
+  $("button").on("mouseup", function(){
+    updateSound(0, $("#forma").val());
+  });
+
+  function updateSound (frequencia, forma){
+      console.log(frequencia, forma);
+
+	  socket.emit('channel', { //emitting a message with 2 parameters"
+		  frequency: frequencia, //parameter 1
+		  type: forma, //parameter 2
+	  });
+  }
+
+//change color on keypress and send data
+$("body").on("keypress", function(e){
+  console.log(e);
+  // alert(e.keyCode);
+    var code = e.keyCode || e.which;
+    switch (code){
+        case (97): //Note C
+            $(".pressed").removeClass('pressed');
+            $("#C").addClass('pressed');
+            updateSound(261.63, $("#forma").val());
+            break;
+        case (119): //Note C#
+            $(".pressed").removeClass('pressed');
+            $("#Csharp").addClass('pressed');
+            updateSound(277.18, $("#forma").val());
+            break;
+        case (115): //Note D
+            $(".pressed").removeClass('pressed');
+            $("#D").addClass('pressed');
+            updateSound(293.66, $("#forma").val());
+            break;
+        case (101)://Note D#
+            $(".pressed").removeClass('pressed');
+            $("#Dsharp").addClass('pressed');
+            updateSound(311.13, $("#forma").val());
+            break;
+        case (100)://Note E
+            $(".pressed").removeClass('pressed');
+            $("#E").addClass('pressed');
+            updateSound(329.63, $("#forma").val());
+            break;
+        case (102)://Note F
+            $(".pressed").removeClass('pressed');
+            $("#F").addClass('pressed');
+            updateSound(349.23, $("#forma").val());
+            break;
+        case (116)://Note F#
+            $(".pressed").removeClass('pressed');
+            $("#Fsharp").addClass('pressed');
+            updateSound(369.99, $("#forma").val());
+            break;
+        case (103)://Note G
+            $(".pressed").removeClass('pressed');
+              $("#G").addClass('pressed');
+            updateSound(392.00, $("#forma").val());
+            break;
+        case (121)://Note G#
+            $(".pressed").removeClass('pressed');
+            $("#Gsharp").addClass('pressed');
+            updateSound(415.30, $("#forma").val());
+            break;
+        case (104)://Note A
+            $(".pressed").removeClass('pressed');
+            $("#A").addClass('pressed');
+            updateSound(440.00, $("#forma").val());
+            break;
+        case (117)://Note A#
+            $(".pressed").removeClass('pressed');
+            $("#Asharp").addClass('pressed');
+            updateSound(466.16, $("#forma").val());
+            break;
+        case (106)://Note B
+            $(".pressed").removeClass('pressed');
+            $("#B").addClass('pressed');
+            updateSound(493.88, $("#forma").val());
+            break;
+    }
+});
+
+
+$("body").on("keyup", function(e){
+$(".pressed").removeClass('pressed');
+updateSound(0, $("#forma").val());
+});
